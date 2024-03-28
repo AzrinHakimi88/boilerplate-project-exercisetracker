86e2f6331da1a9c3de8dd74912a227223ecdb762
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-03-29 03:56:58 +0800

    commit

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.get('/api/users/:_id/logs', async (req, res) => {
       const log = exercises.map((exercise) =>(
              { description: exercise.description,
               duration: exercise.duration,
-              date: exercise.date.toDateString().toString()}
+              date: new Date(exercise.date).toDateString()}
       )) 
 
       res.json({ 
