25ad328efe4943cf2554de194fc026cec84b3861
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-03-29 00:14:34 +0800

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,7 +10,40 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use(express.urlencoded({ extended: true }));
 
+
+mongoose.connect('mongodb://localhost:27017/Exercise').then(() => {console.log('connect to database')})
+
+const userSchema = mongoose.Schema({
+  username : {
+    type : String,
+    required : true
+  }
+})
+
+const userModel = mongoose.model('User', userSchema )
+
+app.post('/api/users',async (req,res) => {
+    const {username} = req.body
+    console.log(username)
+    try{
+      const user = await userModel.create({username: username})
+      res.json({username: user.username, _id : user._id})
+    }
+    catch(err){
+      console.log(err)
+    }
+});
+
+app.get('/api/users',async (req,res) => {
+  try{
+    const user = await userModel.find()
+    res.json(user)
+  }catch(err){
+    console.log(err)
+  }
+})
 
 
 
