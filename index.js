80da93ec6556e18381d89c9ca2ca0e311cfec10d
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-03-29 03:28:23 +0800

    commit

--- index.js
+++ index.js
@@ -105,9 +105,9 @@ app.get('/api/users/:_id/logs', async (req, res) => {
       const exercises = await exercisesQuery.exec();
 
       res.json({ 
-          _id: user._id, 
           username: user.username, 
           count: exercises.length, 
+          _id: user._id, 
           log: exercises.map(exercise => ({
               description: exercise.description,
               duration: exercise.duration,
